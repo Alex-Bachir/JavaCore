83321414f33ee549c6e93279d4882fb37c340c7c
Alex-Bachir <alex.bachir@laplateforme.io>
2026-09-01 20:42:33 +0200

    indentation correction

--- src/javaCore/chapter05Object/exercice/libraryBook/LibraryBook.java
+++ src/javaCore/chapter05Object/exercice/libraryBook/LibraryBook.java
@@ -7,11 +7,11 @@ public class LibraryBook {
         
         LibraryBookObject book1 = new LibraryBookObject();
 
-        book1.name        = "Etre et Temps";
-        book1.author      = "Martin Heidegger";
-        book1.publication = 1927;
+        book1.name        =     "Etre et Temps";
+        book1.author      =  "Martin Heidegger";
+        book1.publication =                1927;
         book1.ISBN        = "978-2-07-070739-3";
-        book1.isBorrow    = false;
+        book1.isBorrow    =               false;
 
         book1.showInformation();
     }
